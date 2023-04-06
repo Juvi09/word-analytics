6eba98d8c02f35dbff1c42c5334c21fe8e910a14
Ntuthuko Ndlovu <ntuthuko.s.ndlovu@gmail.com>
2023-04-06 08:25:36 +0200

    twiiter chars count added

--- script.js
+++ script.js
@@ -1,10 +1,15 @@
 const textareaEl = document.querySelector('.textarea');
+const charactersNumberEl = document.querySelector('.stat__number--characters');
+const twitterNumberEl = document.querySelector('.stat__number--twitter');
+const facebookNumberEl = document.querySelector('.stat__number--facebook');
+const wordNumberEl = document.querySelector('.stat__number--words');
 
 textareaEl.addEventListener('input', function () {
     // ! Determine new number
     const numberOfCharacters = textareaEl.value.length;
+    const twitterCharactersLeft = 280 - numberOfCharacters;
 
     // ! set new number
-    const charactersNumberEl = document.querySelector('.stat__number--characters');
     charactersNumberEl.textContent = numberOfCharacters;
+    twitterNumberEl.textContent = twitterCharactersLeft;
 });
