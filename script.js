1ee6166b84340d1d0bfc864a07a9da4d34e64361
Ntuthuko Ndlovu <ntuthuko.s.ndlovu@gmail.com>
2023-04-06 09:38:55 +0200

    added visual indicator if exceeded limit

--- script.js
+++ script.js
@@ -9,6 +9,19 @@ textareaEl.addEventListener('input', function () {
     const numberOfCharacters = textareaEl.value.length;
     const twitterCharactersLeft = 280 - numberOfCharacters;
     const facebookCharactersLeft = 2200 - numberOfCharacters;
+
+    // ! Show visual indicator if limit is exceeded
+    if (twitterCharactersLeft < 0) {
+        twitterNumberEl.classList.add('stat__number--limit');
+    } else {
+        twitterNumberEl.classList.remove('stat__number--limit');
+    }
+
+    if (facebookCharactersLeft < 0) {
+        facebookNumberEl.classList.add('stat__number--limit');
+    } else {
+        facebookNumberEl.classList.remove('stat__number--limit')
+    }
     // ! set new number
     charactersNumberEl.textContent = numberOfCharacters;
     twitterNumberEl.textContent = twitterCharactersLeft;
